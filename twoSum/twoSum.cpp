d57c95148432a3ef09d80d698ed14f7d8bd2caa5
HenryLu1018 <lhq921018@163.com>
2016-09-17 16:49:33 +0800

    Create twoSum.cpp

---
+++ twoSum/twoSum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i,j; // k = 0;
+        // int p[100]; (不要用数组！)
+        vector<int> p;
+        for (i = 0;i <= nums.size() - 1;i++) // length(nums) - 1 (不要用数组！)
+            for (j = i + 1;j <= nums.size() - 1;j++) 
+                if (nums[i] + nums[j] == target) 
+                { 
+                    // p[k] = i;p[k + 1] = j; (不要用数组！)
+                    p.push_back(i);p.push_back(j); // zero-based indices
+                }
+/*      if (p.size())
+            return p;
+        else
+        {
+            cout<<"No solution!"<<endl; // cout<<p<<endl;（不要用cout！）
+            // return false
+        } 
+*/
+        return p;
+    }
+};
